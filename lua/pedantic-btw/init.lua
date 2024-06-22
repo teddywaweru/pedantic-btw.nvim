b28312da22d89d84ef06a0ed68228c887a63f816
Teddy Njuguna <teddywaweru@gmail.com>
2024-06-21 23:58:10 -0500

    feat: opening buffers using appointed keys

--- lua/pedantic-btw/init.lua
+++ lua/pedantic-btw/init.lua
@@ -10,6 +10,32 @@ M.setup = function()
 end
 M.buffer_list = function()
 	require("bufferlist").display()
+	vim.cmd.redraw()
+
+	M.select_buffer()
+end
+M.select_buffer = function()
+	local char = vim.fn.getcharstr()
+
+	local buffers = 0
+	for _, _ in pairs(Buffers) do
+		buffers = buffers + 1
+	end
+
+	local idx = 0
+	for _, buffer_details in pairs(Buffers) do
+		if char == buffer_details["key"] then
+			vim.api.nvim_buf_delete(0, { force = true })
+			vim.cmd("b " .. buffer_details["buffername"])
+			return
+		else
+			if idx == buffers - 1 then
+				vim.notify("Error matching character with any opened buffer")
+			end
+		end
+
+		idx = idx + 1
+	end
 end
 M.config = function()
 	print("Initialize config")
